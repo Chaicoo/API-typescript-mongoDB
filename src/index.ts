7560f8946773afcc754b89bad38b91ac8afdc3c1
Francisco <gabriellimma27@gmail.com>
2023-05-13 16:30:17 -0300

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import express from "express";
 export * from "colors";
 
-console.log("testando o typescript".bgGreen);
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`🚀 Server running on port ${port}`.blue))
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
